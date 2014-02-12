7b453066e6f538ef0434248a7edc1a264a81f26d
Eric Newport <kethinov@gmail.com>
2014-02-12 06:01:04 -0800

    support for strict mode

--- roosevelt.js
+++ roosevelt.js
@@ -1,3 +1,4 @@
+'use strict';
 var fs = require('fs'),                           // utility library for filesystem access
     path = require('path'),                       // utilities for handling and transforming file paths
     express = require('express'),                 // express http server
@@ -15,7 +16,7 @@ var fs = require('fs'),                           // utility library for filesys
     appDir = os.platform() !== 'win32' ? module.parent.filename.replace(module.parent.filename.split('/')[module.parent.filename.split('/').length - 1], '') : module.parent.filename.replace(module.parent.filename.split('\\')[module.parent.filename.split('\\').length - 1], ''),
 
     // storing contents of package.json for later use
-    package = require(appDir + 'package.json'),
+    pkg = require(appDir + 'package.json'),
 
     // string appended to the end of roosevelt system messages in multithreading mode
     threadSuffix = cluster.worker ? ' (thread ' + cluster.worker.id + ')' : '',
@@ -48,40 +49,40 @@ module.exports = function(params) {
         app.set('appDir', appDir);
 
         // expose package.json
-        package.rooseveltConfig = package.rooseveltConfig || {};
-        app.set('package', package);
+        pkg.rooseveltConfig = pkg.rooseveltConfig || {};
+        app.set('package', pkg);
 
         // set app name from package.json
-        app.set('appName', package.name || 'Roosevelt Express');
+        app.set('appName', pkg.name || 'Roosevelt Express');
 
         // define staticsRoot before other params because other params depend on it
-        params.staticsRoot = params.staticsRoot || package.rooseveltConfig.staticsRoot || 'statics/';
+        params.staticsRoot = params.staticsRoot || pkg.rooseveltConfig.staticsRoot || 'statics/';
         app.set('staticsRoot', path.normalize(params.staticsRoot));
 
         // source remaining params from params argument, then package.json, then defaults
         params = {
-          port: params.port || package.rooseveltConfig.port || process.env.NODE_PORT || 43711,
-          modelsPath: params.modelsPath || package.rooseveltConfig.modelsPath || 'mvc/models/',
-          viewsPath: params.viewsPath || package.rooseveltConfig.viewsPath || 'mvc/views/',
-          controllersPath: params.controllersPath || package.rooseveltConfig.controllersPath || 'mvc/controllers/',
-          notFoundPage: params.notFoundPage || package.rooseveltConfig.notFoundPage || '404.js',
-          internalServerErrorPage: params.internalServerErrorPage || package.rooseveltConfig.internalServerErrorPage || '500.js',
-          serviceUnavailablePage: params.serviceUnavailablePage || package.rooseveltConfig.serviceUnavailablePage || '503.js',
+          port: params.port || pkg.rooseveltConfig.port || process.env.NODE_PORT || 43711,
+          modelsPath: params.modelsPath || pkg.rooseveltConfig.modelsPath || 'mvc/models/',
+          viewsPath: params.viewsPath || pkg.rooseveltConfig.viewsPath || 'mvc/views/',
+          controllersPath: params.controllersPath || pkg.rooseveltConfig.controllersPath || 'mvc/controllers/',
+          notFoundPage: params.notFoundPage || pkg.rooseveltConfig.notFoundPage || '404.js',
+          internalServerErrorPage: params.internalServerErrorPage || pkg.rooseveltConfig.internalServerErrorPage || '500.js',
+          serviceUnavailablePage: params.serviceUnavailablePage || pkg.rooseveltConfig.serviceUnavailablePage || '503.js',
           staticsRoot: params.staticsRoot, // default hierarchy defined above because below params depend on this one being predefined
-          cssPath: params.cssPath || package.rooseveltConfig.cssPath || params.staticsRoot + 'css/',
-          lessPath: params.lessPath || package.rooseveltConfig.lessPath || params.staticsRoot + 'less/',
-          lessCompileWhitelist: params.lessCompileWhitelist || package.rooseveltConfig.lessCompileWhitelist || [],
-          publicFolder: params.publicFolder || package.rooseveltConfig.publicFolder || 'public/',
-          prefixStaticsWithVersion: params.prefixStaticsWithVersion || package.rooseveltConfig.prefixStaticsWithVersion || false,
-          versionNumberLessVar: params.versionNumberLessVar || package.rooseveltConfig.versionNumberLessVar || undefined,
-          publicStatics: params.publicStatics || package.rooseveltConfig.publicStatics || ['css', 'images', 'js'],
-          alwaysHostStatics: params.alwaysHostStatics || package.rooseveltConfig.alwaysHostStatics || false,
-          disableLogger: params.disableLogger || package.rooseveltConfig.disableLogger || false,
-          localhostOnly: params.localhostOnly || package.rooseveltConfig.localhostOnly || true,
-          disableMultipart: params.disableMultipart || package.rooseveltConfig.disableMultipart || false,
-          formidableSettings: params.formidableSettings || package.formidableSettings || {},
-          maxLagPerRequest: params.maxLagPerRequest || package.maxLagPerRequest || 70,
-          shutdownTimeout: params.shutdownTimeout || package.shutdownTimeout || 30000,
+          cssPath: params.cssPath || pkg.rooseveltConfig.cssPath || params.staticsRoot + 'css/',
+          lessPath: params.lessPath || pkg.rooseveltConfig.lessPath || params.staticsRoot + 'less/',
+          lessCompileWhitelist: params.lessCompileWhitelist || pkg.rooseveltConfig.lessCompileWhitelist || [],
+          publicFolder: params.publicFolder || pkg.rooseveltConfig.publicFolder || 'public/',
+          prefixStaticsWithVersion: params.prefixStaticsWithVersion || pkg.rooseveltConfig.prefixStaticsWithVersion || false,
+          versionNumberLessVar: params.versionNumberLessVar || pkg.rooseveltConfig.versionNumberLessVar || undefined,
+          publicStatics: params.publicStatics || pkg.rooseveltConfig.publicStatics || ['css', 'images', 'js'],
+          alwaysHostStatics: params.alwaysHostStatics || pkg.rooseveltConfig.alwaysHostStatics || false,
+          disableLogger: params.disableLogger || pkg.rooseveltConfig.disableLogger || false,
+          localhostOnly: params.localhostOnly || pkg.rooseveltConfig.localhostOnly || true,
+          disableMultipart: params.disableMultipart || pkg.rooseveltConfig.disableMultipart || false,
+          formidableSettings: params.formidableSettings || pkg.formidableSettings || {},
+          maxLagPerRequest: params.maxLagPerRequest || pkg.maxLagPerRequest || 70,
+          shutdownTimeout: params.shutdownTimeout || pkg.shutdownTimeout || 30000,
           onServerStart: params.onServerStart || undefined,
           onReqStart: params.onReqStart || undefined,
           onReqBeforeRoute: params.onReqBeforeRoute || undefined,
@@ -114,7 +115,7 @@ module.exports = function(params) {
         app.set('publicFolder', path.normalize(appDir + params.publicFolder));
 
         // determine statics prefix if any
-        params.staticsPrefix = params.prefixStaticsWithVersion ? package.version || '' : '';
+        params.staticsPrefix = params.prefixStaticsWithVersion ? pkg.version || '' : '';
 
         // ensure 404 page exists
         params.notFoundPage = app.get('controllersPath') + params.notFoundPage;
@@ -135,7 +136,7 @@ module.exports = function(params) {
         app.set('params', params);
 
         // enable gzip compression
-        app.use(express.compress();
+        app.use(express.compress());
 
         // bind user-defined middleware which fires at the beginning of a request if supplied
         if (params.onReqStart && typeof params.onReqStart === 'function') {
@@ -147,7 +148,7 @@ module.exports = function(params) {
       preprocessCSS = function() {
 
         var versionFile = app.get('lessPath') + 'version.less',
-            versionCode = '/* do not edit; generated automatically by Roosevelt */ @' + params.versionNumberLessVar + ': \'' + package.version + '\';',
+            versionCode = '/* do not edit; generated automatically by Roosevelt */ @' + params.versionNumberLessVar + ': \'' + pkg.version + '\';',
             lessFiles = params.lessCompileWhitelist.length ? params.lessCompileWhitelist : wrench.readdirSyncRecursive(app.get('lessPath')),
             parser = new less.Parser({
               paths: app.get('lessPath')
@@ -156,7 +157,7 @@ module.exports = function(params) {
         // make css directory if not present
         if (!fs.existsSync(app.get('cssPath'))) {
           fs.mkdirSync(app.get('cssPath'));
-          console.log(((package.name || 'Roosevelt') + ' making new directory ' + app.get('cssPath').replace(app.get('appDir'), '') + threadSuffix).yellow);
+          console.log(((pkg.name || 'Roosevelt') + ' making new directory ' + app.get('cssPath').replace(app.get('appDir'), '') + threadSuffix).yellow);
         }
 
         // write app version to version.less to force statics versioning
@@ -164,11 +165,11 @@ module.exports = function(params) {
           if (fs.readFileSync(versionFile, 'utf8') !== versionCode) {
             fs.writeFile(versionFile, versionCode, function(err) {
               if (err) {
-                console.error(((package.name || 'Roosevelt') + ' failed to write version.less file!' + threadSuffix).red);
+                console.error(((pkg.name || 'Roosevelt') + ' failed to write version.less file!' + threadSuffix).red);
                 console.error(err);
               }
               else {
-                console.log(((package.name || 'Roosevelt') + ' writing new version.less to reflect new version: ' + package.version + threadSuffix).green);
+                console.log(((pkg.name || 'Roosevelt') + ' writing new version.less to reflect new version: ' + pkg.version + threadSuffix).green);
               }
             });
           }
@@ -178,7 +179,7 @@ module.exports = function(params) {
           (function(file) {
             parser.parse(fs.readFileSync(app.get('lessPath') + file, 'utf8'), function(e, tree) {
               var newFile = app.get('cssPath') + file.replace('.less', '.css');
-              console.log(((package.name || 'Roosevelt') + ' writing new CSS file ' + newFile + threadSuffix).green);
+              console.log(((pkg.name || 'Roosevelt') + ' writing new CSS file ' + newFile + threadSuffix).green);
               fs.writeFileSync(newFile, tree.toCSS({
                 compress: true,
                 yuicompress: true
@@ -275,7 +276,7 @@ module.exports = function(params) {
               form = new formidable.IncomingForm(params.formidableSettings);
               form.parse(req, function(err, fields, files) {
                 if (err) {
-                  console.error(((package.name || 'Roosevelt') + ' failed to parse multipart form at ' + req.url + threadSuffix).red);
+                  console.error(((pkg.name || 'Roosevelt') + ' failed to parse multipart form at ' + req.url + threadSuffix).red);
                   console.error(err);
                   next(err);
                   return;
@@ -295,7 +296,7 @@ module.exports = function(params) {
                               return; // ignore file not found error
                             }
                             else {
-                              console.error(((package.name || 'Roosevelt') + ' failed to remove tmp file: ' + filePath + threadSuffix).red);
+                              console.error(((pkg.name || 'Roosevelt') + ' failed to remove tmp file: ' + filePath + threadSuffix).red);
                               console.error(err);
                             }
                           }
@@ -339,7 +340,7 @@ module.exports = function(params) {
         // make public folder itself if it doesn't exist
         if (!fs.existsSync(publicDir)) {
           fs.mkdirSync(publicDir);
-          console.log(((package.name || 'Roosevelt') + ' making new directory ' + publicDir.replace(app.get('appDir'), '') + threadSuffix).yellow);
+          console.log(((pkg.name || 'Roosevelt') + ' making new directory ' + publicDir.replace(app.get('appDir'), '') + threadSuffix).yellow);
         }
 
         // make statics prefix folder if the setting is enabled
@@ -347,25 +348,25 @@ module.exports = function(params) {
           publicDir += path.normalize(params.staticsPrefix + '/');
           if (!fs.existsSync(publicDir)) {
             fs.mkdirSync(publicDir);
-            console.log(((package.name || 'Roosevelt') + ' making new directory ' + publicDir.replace(app.get('appDir'), '') + threadSuffix).yellow);
+            console.log(((pkg.name || 'Roosevelt') + ' making new directory ' + publicDir.replace(app.get('appDir'), '') + threadSuffix).yellow);
           }
         }
 
         // make symlinks to public statics
-        params.publicStatics.forEach(function(static) {
-          var linkTarget = (appDir + publicDir + static),
-              staticTarget = (appDir + params.staticsRoot + static);
+        params.publicStatics.forEach(function(pubStatic) {
+          var linkTarget = (appDir + publicDir + pubStatic),
+              staticTarget = (appDir + params.staticsRoot + pubStatic);
 
           // make static target folder if it hasn't yet been created
           if (!fs.existsSync(staticTarget)) {
             fs.mkdirSync(staticTarget);
-            console.log(((package.name || 'Roosevelt') + ' making new directory ' + staticTarget.replace(app.get('appDir'), '') + threadSuffix).yellow);
+            console.log(((pkg.name || 'Roosevelt') + ' making new directory ' + staticTarget.replace(app.get('appDir'), '') + threadSuffix).yellow);
           }
 
           // make symlink if it doesn't yet exist
           if (!fs.existsSync(linkTarget) || !fs.lstatSync(linkTarget) || !fs.lstatSync(linkTarget).isSymbolicLink()) {
             fs.symlinkSync(staticTarget, linkTarget, 'junction');
-            console.log(((package.name || 'Roosevelt') + ' making new symlink ').cyan + (linkTarget.replace(app.get('appDir'), '')).yellow + (' pointing to ').cyan + (staticTarget.replace(app.get('appDir'), '')).yellow + (threadSuffix).cyan);
+            console.log(((pkg.name || 'Roosevelt') + ' making new symlink ').cyan + (linkTarget.replace(app.get('appDir'), '')).yellow + (' pointing to ').cyan + (staticTarget.replace(app.get('appDir'), '')).yellow + (threadSuffix).cyan);
           }
         });
 
@@ -380,7 +381,7 @@ module.exports = function(params) {
           controllerFiles = wrench.readdirSyncRecursive(app.get('controllersPath'));
         }
         catch (e) {
-          console.error(((package.name || 'Roosevelt') + ' fatal error: could not load controller files from ' + app.get('controllersPath') + threadSuffix).red);
+          console.error(((pkg.name || 'Roosevelt') + ' fatal error: could not load controller files from ' + app.get('controllersPath') + threadSuffix).red);
           console.error(e);
         }
 
@@ -394,7 +395,7 @@ module.exports = function(params) {
               }
             }
             catch (e) {
-              console.error(((package.name || 'Roosevelt') + ' failed to load controller file: ' + controllerName + '. Please make sure it is coded correctly. See documentation at http://github.com/kethinov/roosevelt for examples.' + threadSuffix).red);
+              console.error(((pkg.name || 'Roosevelt') + ' failed to load controller file: ' + controllerName + '. Please make sure it is coded correctly. See documentation at http://github.com/kethinov/roosevelt for examples.' + threadSuffix).red);
               console.error(e);
             }
           }
@@ -405,20 +406,20 @@ module.exports = function(params) {
           require(params.notFoundPage)(app);
         }
         catch (e) {
-          console.error(((package.name || 'Roosevelt') + ' failed to load 404 controller file: ' + params.notFoundPage + '. Please make sure it is coded correctly. See documentation at http://github.com/kethinov/roosevelt for examples.' + threadSuffix).red);
+          console.error(((pkg.name || 'Roosevelt') + ' failed to load 404 controller file: ' + params.notFoundPage + '. Please make sure it is coded correctly. See documentation at http://github.com/kethinov/roosevelt for examples.' + threadSuffix).red);
           console.error(e);
         }
       },
 
       gracefulShutdown = function() {
         app.set('roosevelt:state', 'disconnecting');
-        console.log(("\n" + (package.name || 'Roosevelt') + ' received kill signal, attempting to shut down gracefully.' + threadSuffix).magenta);
+        console.log(("\n" + (pkg.name || 'Roosevelt') + ' received kill signal, attempting to shut down gracefully.' + threadSuffix).magenta);
         server.close(function() {
-          console.log(((package.name || 'Roosevelt') + ' successfully closed all connections and shut down gracefully.' + threadSuffix).magenta);
+          console.log(((pkg.name || 'Roosevelt') + ' successfully closed all connections and shut down gracefully.' + threadSuffix).magenta);
           process.exit();
         });
         setTimeout(function() {
-          console.error(((package.name || 'Roosevelt') + ' could not close all connections in time; forcefully shutting down.' + threadSuffix).red);
+          console.error(((pkg.name || 'Roosevelt') + ' could not close all connections in time; forcefully shutting down.' + threadSuffix).red);
           process.exit(1);
         }, app.get('params').shutdownTimeout);
       };
@@ -445,7 +446,7 @@ module.exports = function(params) {
           numCPUs = arg;
         }
         else {
-          console.error(((package.name || 'Roosevelt') + ' warning: invalid value "' + array[index + 1] + '" supplied to -cores param.' + threadSuffix).red);
+          console.error(((pkg.name || 'Roosevelt') + ' warning: invalid value "' + array[index + 1] + '" supplied to -cores param.' + threadSuffix).red);
           numCPUs = 1;
         }
       }
@@ -459,12 +460,12 @@ module.exports = function(params) {
       cluster.fork();
     }
     cluster.on('exit', function(worker, code, signal) {
-      console.log(((package.name || 'Roosevelt') + ' thread ' + worker.process.pid + ' died').magenta);
+      console.log(((pkg.name || 'Roosevelt') + ' thread ' + worker.process.pid + ' died').magenta);
     });
   }
   else {
     server = app.listen(app.get('port'), (params.localhostOnly && app.get('env') !== 'development' ? 'localhost' : null), function() {
-      console.log((package.name + ' server listening on port ' + app.get('port') + ' (' + app.get('env') + ' mode)' + threadSuffix).bold);
+      console.log((pkg.name + ' server listening on port ' + app.get('port') + ' (' + app.get('env') + ' mode)' + threadSuffix).bold);
     });
     process.on('SIGTERM', gracefulShutdown);
     process.on('SIGINT', gracefulShutdown);
