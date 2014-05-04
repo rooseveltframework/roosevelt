34396dda35c45f5f244e79655deeca8d96d799e5
Eric Newport <kethinov@gmail.com>
2014-05-03 17:05:43 -0700

    fixing sample app typo

--- sampleApp/mvc/controllers/404.js
+++ sampleApp/mvc/controllers/404.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   app.route('*').all(function(req, res) {
-    var model = require('global')(req, res);
+    var model = require('models/global')(req, res);
     model.content.pageTitle = '{content.appTitle} - 404 not found';
     model.host = req.host;
     model.url = req.url;
--- sampleApp/mvc/controllers/homepage.js
+++ sampleApp/mvc/controllers/homepage.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   app.route('/').get(function(req, res) {
-    var model = require('global')(req, res);
+    var model = require('models/global')(req, res);
     model.content.hello = 'Hi! I\'m a variable trickling down through the MVC structure!';
     model.content.picLabel = 'Here\'s a silly picture of Teddy Roosevelt:';
     model.teddyPath = '/images/teddy.jpg';
