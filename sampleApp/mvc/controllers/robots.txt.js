253692a8be673c7b89a8773bdefb09b25fc06a2a
Eric Newport <kethinov@gmail.com>
2014-05-03 17:30:39 -0700

    fix for typo in sample app

--- sampleApp/mvc/controllers/robots.txt.js
+++ sampleApp/mvc/controllers/robots.txt.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     res.setHeader('Content-Type', 'text/plain');
 
     // it's plain text, so we don't want to render it with the template parser
-    fs.readFile(app.get(viewsPath) + 'robots.txt', 'utf8', function(err, data) {
+    fs.readFile(app.get('viewsPath') + 'robots.txt', 'utf8', function(err, data) {
       res.send(data);
     });
   });
