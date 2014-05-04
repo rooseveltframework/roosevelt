c4911f4ab7bbdad60befed57716ba305d1b90d03
Eric Newport <kethinov@gmail.com>
2014-05-03 17:58:11 -0700

    more intuitive cli tool

--- cli.js
+++ cli.js
@@ -11,8 +11,8 @@ var fs = require('fs'),
       console.log("Roosevelt MVC web framework\n");
       console.log("Version " + package.version + "\n");
       console.log("USAGE:");
-      console.log("roosevelt create .                     create sample roosevelt app in current working directory");
-      console.log("roosevelt create /path/to/somewhere    create sample roosevelt app in /path/to/somewhere");
+      console.log("roosevelt create appName                       create sample roosevelt app called appName in your current working directory");
+      console.log("roosevelt create /path/to/somewhere/appName    create sample roosevelt app in /path/to/somewhere called appName");
     };
 
 if (notifier.update) {
@@ -20,19 +20,18 @@ if (notifier.update) {
 }
 
 if (cmd && arg) {
-  if (arg === '.') {
-    arg = process.cwd();
-  }
   if (cmd === 'create') {
     try {
       wrench.copyDirSyncRecursive(path.normalize(__dirname + '/sampleApp/'), path.normalize(arg), {
-        forceDelete: true, // Whether to overwrite existing directory or not
+        forceDelete: false, // Whether to overwrite existing directory or not
         excludeHiddenUnix: false, // Whether to copy hidden Unix files or not (preceding .)
         preserveFiles: true, // If we're overwriting something and the file already exists, keep the existing
         preserveTimestamps: false, // Preserve the mtime and atime when copying files
         inflateSymlinks: false // Whether to follow symlinks or not when copying files
       });
-      fs.renameSync(path.normalize(arg + '/.npmignore'), path.normalize(arg + '/.gitignore')); // fix to compensate for this "feature" https://github.com/npm/npm/issues/1862
+      if (fs.existsSync(path.normalize(arg + '/.npmignore'))) {
+        fs.renameSync(path.normalize(arg + '/.npmignore'), path.normalize(arg + '/.gitignore')); // fix to compensate for this "feature" https://github.com/npm/npm/issues/1862
+      }
     }
     catch (e) {
       console.error(e);
