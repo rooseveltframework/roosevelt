6e35a312dfb88225a85f375af48cabde7e2d70c8
Johnny Siu <jxs7205@rit.edu>
2018-01-16 15:30:57 -0500

    changed the app.js file and made comments into describe blocks

--- test/lib/app.js
+++ test/lib/app.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const appDir = path.join(__dirname, '../app/commandLineTest')
 const app = require('../../../roosevelt')({
-    appDir: appDir,
-    generateFolderStructure: false,
+  appDir: appDir,
+  generateFolderStructure: false
 })
 if (process.send) {
-    process.send(app.expressApp.get('params'))
+  process.send(app.expressApp.get('params'))
+  process.exit()
 }
-process.exit()
