fcc656a2068f67153bdcc4ec40553457cce6e843
Johnny Siu <jxs7205@rit.edu>
2018-01-18 11:38:34 -0500

    BROKEN: can't delete temp files

--- test/unit/jsTest.js
+++ test/unit/jsTest.js
@@ -4,26 +4,24 @@ const assert = require('assert')
 const fs = require('fs')
 const fse = require('fs-extra')
 const path = require('path')
-const cleanupTestApp = require('../util/cleanupTestApp')
-const fork = require('child_process').fork
+// const cleanupTestApp = require('../util/cleanupTestApp')
 const klawSync = require('klaw-sync')
 
-describe('JavaScript Section Tests', function () {
-  const appDir = path.join(__dirname, '../app/jsApp')
-  const appDir2 = path.join(__dirname, '../app/JSTest')
+describe('JavaScript Section Test', function () {
+  const appDir = path.join(__dirname, '../app/jsTest')
   const test1 = 'var a = 7'
   const test2 = 'var b = "turkey"'
   const test3 = 'var c = true'
   let operations = []
   let pathsOfStaticJS = [
-    path.join(appDir2, 'statics', 'js', 'a.js'),
-    path.join(appDir2, 'statics', 'js', 'b.js'),
-    path.join(appDir2, 'statics', 'js', 'c.js')
+    path.join(appDir, 'statics', 'js', 'a.js'),
+    path.join(appDir, 'statics', 'js', 'b.js'),
+    path.join(appDir, 'statics', 'js', 'c.js')
   ]
   let pathsOfCompiledJS = [
-    path.join(path.join(appDir2, 'statics', '.build', 'js', 'a.js')),
-    path.join(path.join(appDir2, 'statics', '.build', 'js', 'b.js')),
-    path.join(path.join(appDir2, 'statics', '.build', 'js', 'c.js'))
+    path.join(path.join(appDir, 'statics', '.build', 'js', 'a.js')),
+    path.join(path.join(appDir, 'statics', '.build', 'js', 'b.js')),
+    path.join(path.join(appDir, 'statics', '.build', 'js', 'c.js'))
   ]
   let staticJSFiles = [
     test1,
@@ -31,10 +29,21 @@ describe('JavaScript Section Tests', function () {
     test3
   ]
 
-  before(function (done) {
-    fse.ensureDirSync(path.join(appDir))
-    fse.ensureDirSync(path.join(appDir2, 'statics', 'js'))
+  after(function (done) {
+  /*
+    cleanupTestApp(appDir, (err) => {
+      if (err) {
+        throw err
+      } else {
+        done()
+      }
 
+    })
+  */
+    done()
+  })
+  it('should compile all static js files using roosevelt-uglify', function (done) {
+    fse.ensureDirSync(path.join(appDir, 'statics', 'js'))
     for (let x = 0; x < pathsOfStaticJS.length; x++) {
       operations.push(new Promise((resolve, reject) => {
         fs.writeFile(pathsOfStaticJS[x], staticJSFiles[x], (error) => {
@@ -42,51 +51,43 @@ describe('JavaScript Section Tests', function () {
             reject(error)
           } else {
             resolve()
+            console.log('resolved')
           }
         })
       }))
     }
-
-    operations.push(new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, '../util/JSapp.js'), (err, buffer) => {
-        if (err) {
-          reject(err)
-        }
-        fs.writeFile(path.join(appDir, 'app.js'), buffer.toString(), (error) => {
-          if (error) {
-            reject(error)
-          } else {
-            resolve()
-          }
-        })
-      })
-    }))
-
     Promise.all(operations).then(() => {
-      done()
-    })
-  })
-
-  after(function (done) {
-    cleanupTestApp(appDir, (err) => {
-      if (err) {
-        throw err
-      } else {
-        done()
-      }
-    })
-  })
-
-  it('should compile all static js files using roosevelt-uglify', function (done) {
-    const testApp = fork(path.join(appDir, 'app.js'))
-    testApp.on('close', () => {
-      const compiledJS = path.join(path.join(appDir2, 'statics', '.build', 'js'))
-      const compiledJSArray = klawSync(compiledJS)
-      compiledJSArray.forEach((file) => {
-        let test = pathsOfCompiledJS.includes(file.path)
-        assert.equal(test, true)
+      console.dir(operations)
+      console.log('done writing, going to start server')
+      const app = require('../../../roosevelt')({
+        appDir: appDir,
+        generateFolderStructure: true,
+        suppressLogs: {
+          httpLogs: true,
+          rooseveltLogs: true,
+          rooseveltWarnings: true
+        },
+        js: {
+          compiler: {
+            nodeModule: 'roosevelt-uglify',
+            showWarnings: false,
+            params: {}
+          },
+          output: '.build/js'
+        }
       })
-      done()
+
+      app.initServer(() => {
+        console.log('inited server')
+        const compiledJS = path.join(path.join(appDir, 'statics', '.build', 'js'))
+        const compiledJSArray = klawSync(compiledJS)
+        compiledJSArray.forEach((file) => {
+          let test = pathsOfCompiledJS.includes(file.path)
+          console.log(test)
+          assert.equal(test, true)
+        })
+        done()
+      })
     })
   })
 })
