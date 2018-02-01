4ed2e83e6589e6ede6c740499cc209433ce35ae3
Johnny Siu <jxs7205@rit.edu>
2018-02-01 10:01:37 -0500

    changed the regex

--- test/util/cleanupTestApp.js
+++ test/util/cleanupTestApp.js
@@ -2,7 +2,7 @@ const fse = require('fs-extra')
 
 module.exports = function (appDir, next) {
   // use regexp to make sure that the appDir included 'roosvelt' with any characters following it follow by '/test/app'
-  if (/roosevelt.*\\test\\app/.test(appDir)) {
+  if (/roosevelt.*[\\/]test[\\/]app/.test(appDir)) {
     fse.remove(appDir, (err) => {
       if (err) {
         next(err)
