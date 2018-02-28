8c2e9aad9e4be9bcd68a1926b57a3fe0562bdd9e
Johnny Siu <jxs7205@rit.edu>
2018-02-28 10:35:26 -0500

    comments to explain what the test.inventoryNum is doing there

--- test/util/errController.js
+++ test/util/errController.js
@@ -1,5 +1,5 @@
 let test = {}
-
+// test.inventoryNum is there to help test that Roosevelt will throw an error if the controller is coded incorrectly
 module.exports = (app) => {
   app.route('/err').get(test.inventoryNum, (req, res) => {
     let count = test.inventoryNum + 1
