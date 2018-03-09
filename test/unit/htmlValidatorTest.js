441c19c22ee99003e78fb39f43dd09e638be3db7
Johnny Siu <jxs7205@rit.edu>
2018-03-09 16:01:37 -0500

    increased the port number

--- test/unit/htmlValidatorTest.js
+++ test/unit/htmlValidatorTest.js
@@ -706,7 +706,7 @@ describe('Roosevelt HTML Validator Test', function () {
       appDir: appDir,
       htmlValidator: {
         enable: true,
-        port: 1000,
+        port: 2000,
         separateProcess: true
       },
       onServerStart: `(app) => {process.send(app.get("params"))}`
