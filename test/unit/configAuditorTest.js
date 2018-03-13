50fb4694d155057651c266b3257fd68bf7454af4
Johnny Siu <jxs7205@rit.edu>
2018-03-13 15:10:07 -0400

    added a check to make sure that the config Auditor would report back that the package.json file is fine if it has no errors in it

--- test/unit/configAuditorTest.js
+++ test/unit/configAuditorTest.js
@@ -471,4 +471,43 @@ describe('Roosevelt config Auditor Test', function () {
       done()
     })
   })
+
+  it('should report that there are no errors found if there are no errors', function (done) {
+    // bool var to hold whether or not the right logs were outputted
+    let startingConfigAuditBool = false
+    let noErrorsBool = false
+
+    // generate the package.json file
+    fse.ensureDirSync(appDir)
+    let content = fse.readFileSync(path.join(appDir, '../', '../', 'util', 'configAuditpackage5.json'))
+    fse.writeFileSync(path.join(appDir, 'package.json'), content)
+
+    // generate the app.js file
+    generateTestApp({
+      appDir: appDir,
+      onServerStart: `(app) => {process.send("something")}`
+    }, options)
+
+    // fork the app.js file and run it as a child process
+    const testApp = fork(path.join(appDir, 'app.js'), {'stdio': ['pipe', 'pipe', 'pipe', 'ipc']})
+
+    testApp.stdout.on('data', (data) => {
+      if (data.includes('Starting roosevelt user configuration audit...')) {
+        startingConfigAuditBool = true
+      }
+      if (data.includes('Configuration audit completed with no errors found.')) {
+        noErrorsBool = true
+      }
+    })
+
+    testApp.on('message', () => {
+      testApp.kill('SIGINT')
+    })
+
+    testApp.on('exit', () => {
+      assert.equal(startingConfigAuditBool, true, 'Roosevelt did not start the config Auditor')
+      assert.equal(noErrorsBool, true, 'config Auditor is reporting back that there is an error even though the package.json file does not have one')
+      done()
+    })
+  })
 })
