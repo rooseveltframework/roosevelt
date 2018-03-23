0b864d6dc9d23659d9aef619edf8c949b94a5969
Johnny Siu <jxs7205@rit.edu>
2018-03-23 11:42:40 -0400

    added a test to check if Roosevelt can also run on localhostOnly and production if https was enabled

--- test/unit/rooseveltTest.js
+++ test/unit/rooseveltTest.js
@@ -484,4 +484,48 @@ describe('Roosevelt roosevelt.js Section Tests', function () {
       done()
     })
   })
+
+  it('should be able to run the app even with the localhostOnly param set to true, in production mode and run a https server', function (done) {
+    // bool var to hold whether a specific log was outputted
+    let productionModeBool = false
+    let httpsServerMadeBool = false
+
+    // generate a app.js file
+    generateTestApp({
+      appDir: appDir,
+      generateFolderStructure: true,
+      localhostOnly: true,
+      https: {
+        enable: true,
+        httpsPort: 43203
+      },
+      onServerStart: `(app) => {process.send(app.get("params"))}`
+    }, sOptions)
+
+    // fork the app and run it as a child process
+    const testApp = fork(path.join(appDir, 'app.js'), ['--prod'], {'stdio': ['pipe', 'pipe', 'pipe', 'ipc']})
+
+    // test to see if the app is being run in production mode
+    testApp.stdout.on('data', (data) => {
+      if (data.includes('Starting Roosevelt Express in production mode...')) {
+        productionModeBool = true
+      }
+      if (data.includes('Roosevelt Express HTTPS server listening on port')) {
+        httpsServerMadeBool = true
+      }
+    })
+
+    // when the app starts, check that localhostOnly was set correctly and then kill it
+    testApp.on('message', (params) => {
+      assert.equal(params.localhostOnly, true, 'Roosevelt did not set localhostOnly to true')
+      testApp.kill('SIGINT')
+    })
+
+    // on exit, see if the app started in production mode
+    testApp.on('exit', () => {
+      assert.equal(productionModeBool, true, 'Roosevelt did not start in production mode even though the production flag was passed to it as a command line arg')
+      assert.equal(httpsServerMadeBool, true, 'Roosevelt did not make a HTTPS server even thought it is enabled')
+      done()
+    })
+  })
 })
