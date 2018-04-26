f6c973e824b0f497ef70983439dd512fab7761b2
Johnny Siu <jxs7205@rit.edu>
2018-04-26 14:25:12 -0400

    Got rid of the original way of checking if java was installed and implemented a new way by checking if we get the correct response for java -version and made a test for it

--- test/unit/htmlValidatorTest.js
+++ test/unit/htmlValidatorTest.js
@@ -880,6 +880,42 @@ describe('Roosevelt HTML Validator/ Kill Validator Test', function () {
         done()
       })
     })
+
+    it('should give instruction to install java if the validator is called without having java installed on the machine', function (done) {
+      // bool var to hold whether or not a specific error was triggered
+      let javaEnonetErrorBool = false
+
+      // generate the app.js file
+      generateTestApp({
+        appDir: appDir,
+        generateFolderStructure: true,
+        onServerStart: `(app) => {process.send(app.get("params"))}`,
+        htmlValidator: {
+          enable: true
+        }
+      }, options)
+
+      // fork the app and run it as a child process
+      const testApp = fork(path.join(appDir, 'app.js'), ['--dev'], {'stdio': ['pipe', 'pipe', 'pipe', 'ipc'], env: {PATH: 'sfsfsff'}})
+
+      // on error logs, check to see if any of them are the error log that we want
+      testApp.stderr.on('data', (data) => {
+        if (data.includes('Error: You must install Java to continue. HTML validation disabled, error on initialization (check to make sure Java is installed and in your path)')) {
+          javaEnonetErrorBool = true
+        }
+      })
+
+      // when the app finishes initialization, kill it
+      testApp.on('message', () => {
+        testApp.kill('SIGINT')
+      })
+
+      // when the app is exiting, see if the error was hit
+      testApp.on('exit', () => {
+        assert.equal(javaEnonetErrorBool, true, 'The Path does not point to java, so we should get an error saying that the command of java was not recognised')
+        done()
+      })
+    })
   })
 
   describe('Roosevelt killValidator test', function () {
