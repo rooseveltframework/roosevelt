27ff1b0fa22f5c118aa1e7cb496720202d949950
siuster95 <jxs7205@rit.edu>
2018-04-28 11:09:22 -0700

    HTMLValidator 100% part 2  (#430)
    * Made an error on the brokenHTMLTest.html file so when it gives back data on the error, it will have a firstline var
    
    * getting rid of isError as it is no longer helping us in the script
    
    * got rid of the try catch block in htmlValidator
    
    * changed how htmlValidator is recognizing warnings so tha the line is covered
    
    * added some other parts to the brokenHTMLTest html file to see if this will give me a message that has type info but not subType of warning
    
    * changed the htmlValidator so that it will put everything else that is not an error as a warning. (Since all info messages seem to have the sub-type of warnings)
    
    * made a test to check that Roosevelt would say that it couldn't init the validator if it timed out and changed generateTestApp so that it will help simulate time to make the validator time init process out
    
    * added a kill to when the HTML Validator times out, so that the process is shut down completely and doesn't start a htmlValidator when it is not suppose to
    
    * added comments to the timeout test and made it so that the validator param is disabled and that the warning tells the user that the validator has been disabled due to timeout in the htmlValidator timeout part
    
    * Got rid of the original way of checking if java was installed and implemented a new way by checking if we get the correct response for java -version and made a test for it
    
    * fixed standard error
    
    * got rid of the error check on the spawn process as their would probably not be any errors on the command out of java not being installed onto the machine

--- test/util/generateTestApp.js
+++ test/util/generateTestApp.js
@@ -7,7 +7,15 @@ const util = require('util')
 
 module.exports = function (params, options) {
   const appDir = params.appDir || options.appDir
-  let appJSContents = `const app = require(\`${options.rooseveltPath}\`)(${util.inspect(params, {depth: null})})\n\n`
+  let appJSContents = ''
+
+  if (options.msgEnabled) {
+    appJSContents += `const sinon = require('sinon')\n`
+    appJSContents += `let config = {shouldAdvanceTime: true}\n`
+    appJSContents += `let clock = sinon.useFakeTimers(config)\n\n`
+  }
+
+  appJSContents += `const app = require(\`${options.rooseveltPath}\`)(${util.inspect(params, {depth: null})})\n\n`
   let defaultMessages = 'process.send(app.expressApp.get(\'params\'))'
   appJSContents = appJSContents.replace(/('\()/g, '(')
   appJSContents = appJSContents.replace(/(\}')/g, '}')
@@ -23,6 +31,13 @@ module.exports = function (params, options) {
     }
   } else {
     appJSContents += defaultMessages
+  }
+
+  if (options.msgEnabled) {
+    appJSContents += `\n\nprocess.on('message', function (){\n`
+    appJSContents += `console.log('msg recieved')\n`
+    appJSContents += `clock.tick(30000)\n`
+    appJSContents += `})`
   }
 
   if (options.stopServer) {
