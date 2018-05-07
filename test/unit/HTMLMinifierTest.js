e721cb9bd9c5d5cf4de80d7c4e32ad7118841359
Johnny Siu <jxs7205@rit.edu>
2018-05-07 14:01:53 -0400

    added some more test to HTMLMinifierTest

--- test/unit/HTMLMinifierTest.js
+++ test/unit/HTMLMinifierTest.js
@@ -51,10 +51,9 @@ describe('Roosevelt HTML minifier Section test', function () {
         .get('/teddyTest')
         .expect(200, (err, res) => {
           if (err) {
-            console.log(err)
+            assert.fail(err.message)
             testApp.kill('SIGINT')
           }
-          console.log(res.text)
           let test1 = res.text.includes('<!DOCTYPE html><html lang=en><head>')
           let test2 = res.text.includes('<title>Teddy Test</title></head><body><h1>Heading Test</h1><p>This is the first sentence that I am grabbing from my teddy model</p>')
           assert.equal(test1, true, 'Roosevelt minifier did not minify a part of the HTML')
@@ -94,10 +93,52 @@ describe('Roosevelt HTML minifier Section test', function () {
         .get('/teddyTest')
         .expect(200, (err, res) => {
           if (err) {
-            console.log(err)
+            assert.fail(err.message)
             testApp.kill('SIGINT')
           }
-          console.log(res.text)
+          let test1 = res.text.includes('<!-- This is a test comment 1 -->')
+          let test2 = res.text.includes('<!DOCTYPE html><html lang=en><head>')
+          let test3 = res.text.includes('<p id=" ">')
+          assert.equal(test1, true, 'Roosevelt minifier change its minifier option with what was given in params')
+          assert.equal(test2, false, 'Roosevelt minifier change its minifier option with what was given in params')
+          assert.equal(test3, true, 'Roosevelt minifier change its minifier option with what was given in params')
+          testApp.kill('SIGINT')
+        })
+    })
+
+    testApp.on('exit', () => {
+      done()
+    })
+  })
+
+  it('should skip Minifying the HTML if noMinify is set to true', function (done) {
+    generateTestApp({
+      appDir: appDir,
+      generateFolderStructure: true,
+      onServerStart: `(app) => {process.send(app.get("params"))}`,
+      noMinify: true,
+      viewEngine: [
+        'html: teddy'
+      ]
+    }, options)
+
+    // fork the app and run it as a child process
+    const testApp = fork(path.join(appDir, 'app.js'), ['--prod'], {'stdio': ['pipe', 'pipe', 'pipe', 'ipc']})
+
+    testApp.on('message', (params) => {
+      request(`http://localhost:${params.port}`)
+        .get('/teddyTest')
+        .expect(200, (err, res) => {
+          if (err) {
+            assert.fail(err.message)
+            testApp.kill('SIGINT')
+          }
+          let test1 = res.text.includes('<!-- This is a test comment 1 -->')
+          let test2 = res.text.includes('<!DOCTYPE html><html lang=en><head>')
+          let test3 = res.text.includes('<p id=" ">')
+          assert.equal(test1, true, 'Roosevelt used its Minifier even though noMinify is set to true')
+          assert.equal(test2, false, 'Roosevelt used its Minifier even though noMinify is set to true')
+          assert.equal(test3, true, 'Roosevelt used its Minifier even though noMinify is set to true')
           testApp.kill('SIGINT')
         })
     })
