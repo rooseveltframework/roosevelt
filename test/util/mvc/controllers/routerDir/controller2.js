501c77df4454deb5358ed9810c414cd4ea8deac9
Mike Mulligan <mkmllgn@gmail.com>
2019-01-09 14:00:33 -0500

    Clean up test files

--- test/util/mvc/controllers/routerDir/controller2.js
+++ test/util/mvc/controllers/routerDir/controller2.js
@@ -1,10 +1,5 @@
 module.exports = (app) => {
-  const path = require('path')
-  // route to request the plain HTML page and return the HTML page
   app.route('/controller2').get((req, res) => {
-    // save the path of the plain HTML page
-    let htmlPath = path.join(__dirname, '../../views/plainHTMLTest.html')
-    // send the plain HTML page back
-    res.sendFile(htmlPath)
+    res.send('test')
   })
 }
