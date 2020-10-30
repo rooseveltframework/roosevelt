684a6141f8c2bf2747bd06f269fa53c60666b392
Eric Newport <kethinov@gmail.com>
2020-10-30 17:34:53 -0400

    0.19.4 (#976)
    * 0.19.4
    
    - Fixed bug that caused Roosevelt to not listen to `NODE_ENV`.
    - Moved symlink creation to a separate step and made happen earlier in the app initialization process to maek it easier to work with the various Roosevelt server starting events.
    - Various dependencies bumped.
    
    * linter fixes

--- test/util/mvc/controllers/longConn.js
+++ test/util/mvc/controllers/longConn.js
@@ -9,7 +9,7 @@ module.exports = (router) => {
   router.route('/slow').get((req, res) => {
     const start = new Date()
     while ((new Date() - start) < 250) {
-      for (var i = 0; i < 1e5;) i++
+      for (let i = 0; i < 1e5;) i++
     }
     res.send('done')
   })
