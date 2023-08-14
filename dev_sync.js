7e6fa41de4d324bde5e1957ddf3e87c14f1d143e
Railon Acosta <cagedtreestechnology@gmail.com>
2023-08-14 11:41:21 -0400

    Need to double check if I even need the watcher dependancy, rsync seams to be working as intended

--- dev_sync.js
+++ dev_sync.js
@@ -5,23 +5,7 @@ const Rsync = require('rsync')
 const DEST_DIR = './../my-roosevelt-sample-app'
 const SRC_DIR = __dirname
 
-const rsync = new Rsync()
-//   .shell('ssh')
-  .flags('avz')
-  .delete()
-  .exclude('.DS_Store')
-  .source(`${DEST_DIR}/node_modules/roosevelt/`)
-  .destination(SRC_DIR)
-
-rsync.execute(function (error, code, cmd) {
-  console.log(code)
-  console.log(cmd)
-
-  if (error) {
-    console.error(`error: ${error.message}`)
-  }
-})
-// const fs = require('fs')
+const fs = require('fs')
 // const watcher = require('watcher')
 
 // async function fsWatch () {
@@ -36,48 +20,46 @@ rsync.execute(function (error, code, cmd) {
 // console.log(`*** DEST_DIR:  ${DEST_DIR}`)
 // console.log(fsWatch())
 
-// try {
-//   if (DEST_DIR === '') {
-//     this.logger.error('ERROR: DEST_DIR is an empty variable')
-//   } else if (DEST_DIR === SRC_DIR) {
-//     this.logger.error('ERROR: DEST_DIR is pointing to the same path as SRC_DIR ')
-//   } else {
-//     if (fs.existsSync(`${DEST_DIR}/node_modules/roosevelt/`)) {
-//       this.logger.info('💭', 'We are in a Roosevelt app ...')
-//       fsWatch()
-//     } else {
-//       this.logger.info('')
-//       this.logger.warn('Make sure the above directories are correct or this could delete unwanted files!')
-//       this.logger.info('💭', 'We are not in a Roosevelt app ...')
-//       this.logger.info('')
-//       fsWatch()
-//     }
-//   }
-// } catch (err) { console.log(err) }
+try {
+  if (DEST_DIR === '') {
+    this.logger.error('ERROR: DEST_DIR is an empty variable')
+  } else if (DEST_DIR === SRC_DIR) {
+    this.logger.error('ERROR: DEST_DIR is pointing to the same path as SRC_DIR ')
+  } else {
+    if (fs.existsSync(`${DEST_DIR}/node_modules/roosevelt/`)) {
+      this.logger.info('💭', 'We are in a Roosevelt app ...')
+      fsWatch()
+    } else {
+      this.logger.info('')
+      this.logger.warn('Make sure the above directories are correct or this could delete unwanted files!')
+      this.logger.info('💭', 'We are not in a Roosevelt app ...')
+      this.logger.info('')
+      fsWatch()
+    }
+  }
+} catch (err) { console.log(err) }
 
-// function fsWatch () {
-//   const Logger = require('roosevelt-logger')
-//   this.logger = new Logger()
-//   const { execSync } = require('child_process')
+function fsWatch () {
+  const Logger = require('roosevelt-logger')
+  this.logger = new Logger()
+  //   const { execSync } = require('child_process')
+  const rsync = new Rsync()
+  //   .shell('ssh')
+    .flags('avz')
+    .delete()
+    .exclude('.DS_Store')
+    .source(`${DEST_DIR}/node_modules/roosevelt/`)
+    .destination(SRC_DIR)
 
-//   this.logger.info('💭', 'Roosevelt fswatch rsync tool running...')
-//   this.logger.info('')
-//   this.logger.info('💭', `Now watching: ${SRC_DIR}`)
-//   this.logger.info('💭', `Will copy to: ${DEST_DIR}`)
-//   this.logger.info('')
+  rsync.execute(function (error, code, cmd) {
+    if (error) {
+      console.error(`error: ${error.message}`)
+    }
+  })
 
-//   execSync(`fswatch -0 ${SRC_DIR} | while read -d "" event
-//   do
-//     rsync -avz --delete --exclude=.DS_Store ${SRC_DIR}/ ${DEST_DIR}/node_modules/roosevelt/
-//   done`, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`error: ${error.message}`)
-//       return
-//     }
-//     if (stderr) {
-//       console.error(`stderr: ${stderr}`)
-//       return
-//     }
-//     console.log(`stdout:\n${stdout}`)
-//   })
-// }
+  this.logger.info('💭', 'Roosevelt fswatch rsync tool running...')
+  this.logger.info('')
+  this.logger.info('💭', `Now watching: ${SRC_DIR}`)
+  this.logger.info('💭', `Will copy to: ${DEST_DIR}`)
+  this.logger.info('')
+}
