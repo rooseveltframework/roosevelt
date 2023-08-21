05f808d85b788a31847cc7383a310943f966f4a0
Railon Acosta <cagedtreestechnology@gmail.com>
2023-08-21 10:00:59 -0400

    Script will copy and past any changes you have made to roosevelt before running the script to /node_modules/roosevelt

--- dev_sync.js
+++ dev_sync.js
@@ -1,9 +1,14 @@
 const Logger = require('roosevelt-logger')
 this.logger = new Logger()
-// const Rsync = require('rsync')
-const DEST_DIR = process.env.DEST_DIR
+const Rsync = require('rsync')
+let DEST_DIR = process.env.DEST_DIR
+// let DEST_DIR = './../my-roosevelt-sample-app'
+rsvtConfig = `${DEST_DIR}/rooseveltConfig.json`
+DEST_DIR = `${DEST_DIR}/node_modules/`
+
 const SRC_DIR = __dirname
 const fs = require('fs')
+console.log(`SRC_DIR: ${SRC_DIR}`)
 
 try {
   if (DEST_DIR === '' || DEST_DIR === undefined) {
@@ -11,7 +16,7 @@ try {
   } else if (DEST_DIR === SRC_DIR) {
     this.logger.error('ERROR: DEST_DIR is pointing to the same path as SRC_DIR ')
   } else {
-    if (fs.existsSync(`${DEST_DIR}/node_modules/roosevelt/`)) {
+    if (fs.existsSync(rsvtConfig) || fs.existsSync(`${DEST_DIR}/roosevelt/`)) {
       this.logger.info('💭', 'We are in a Roosevelt app ...')
       fsWatch()
     } else {
@@ -19,7 +24,6 @@ try {
       this.logger.warn('Make sure the above directories are correct or this could delete unwanted files!')
       this.logger.info('💭', 'We are not in a Roosevelt app ...')
       this.logger.info('')
-      fsWatch()
     }
   }
 } catch (err) { console.log(err) }
@@ -29,38 +33,40 @@ async function fsWatch () {
   this.logger = new Logger()
   const watch = await import('watcher')
   const Watcher = watch.default
-  const watcher = new Watcher(DEST_DIR)
+  const watcher = new Watcher(SRC_DIR)
+
+
+  const rsync = new Rsync()
+    .flags('avz')
+    .delete()
+    .exclude('.DS_Store')
+    .source(SRC_DIR)
+    .destination(DEST_DIR)
 
   watcher.on('error', error => {
     this.logger.err(error)
   })
-
   watcher.on('ready', () => {
     this.logger.info('💭', 'Roosevelt fswatch rsync tool running...')
     this.logger.info('')
     this.logger.info('💭', `Now watching: ${SRC_DIR}`)
     this.logger.info('💭', `Will copy to: ${DEST_DIR}`)
     this.logger.info('')
-  })
 
-  process.on('SIGINT', function () {
-    console.log('')
+    rsync.execute(function (error, code, cmd) {
+      if (error) {
+        this.logger.error(`ERROR: ${error.message}`)
+      }
+    })
+
     console.log('💭')
-    console.log('💭 Caught interrupt signal')
+    console.log('💭 Closing rsync')
+    console.log('💭 Closing fswatch')
+
     console.log('💭')
     watcher.close()
     process.exit()
+
   })
 }
 
-// const rsync = new Rsync()
-//   .flags('avz')
-//   .delete()
-//   .exclude('.DS_Store')
-//   .source(`${DEST_DIR}/node_modules/roosevelt/`)
-//   .destination(SRC_DIR)
-// rsync.execute(function (error, code, cmd) {
-//   if (error) {
-//     this.logger.error(error.message)
-//   }
-// })
