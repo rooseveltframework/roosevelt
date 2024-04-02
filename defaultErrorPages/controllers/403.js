dcf3dfb96573c4274fd2ee23f9c5238b7164e165
Alex Widener <awidener3@gmail.com>
2024-04-02 09:01:12 -0400

    add comments

--- defaultErrorPages/controllers/403.js
+++ defaultErrorPages/controllers/403.js
@@ -4,16 +4,16 @@ const template = require('es6-template-strings')
 const errorPage = fs.readFileSync(path.join(__dirname, '../views/403.html'))
 
 module.exports = function (app, req, res) {
-  app.route('/403').all(function (req, res) {
-    const model = {
-      url: req.url,
-      mainDomain: req.headers['x-forwarded-host'] || req.headers.host,
-      appVersion: app.get('appVersion')
-    }
-    const errorTemplate = template(errorPage, model)
+  // app.route('/403').all(function (req, res) {
+  const model = {
+    url: req.url,
+    mainDomain: req.headers['x-forwarded-host'] || req.headers.host,
+    appVersion: app.get('appVersion')
+  }
+  const errorTemplate = template(errorPage, model)
 
-    res.setHeader('Connection', 'close')
-    res.status(403)
-    res.send(errorTemplate)
-  })
+  res.setHeader('Connection', 'close')
+  res.status(403)
+  res.send(errorTemplate)
+  // })
 }
