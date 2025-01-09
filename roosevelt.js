e3bbf80373a1acd7348fd744fe1e480dbc870ae9
Troy Coutu <Autre31415@gmail.com>
2025-01-09 17:04:13 -0500

    start reorganizing the constructor vs init/start concepts

--- roosevelt.js
+++ roosevelt.js
@@ -2,11 +2,13 @@ require('@colors/colors')
 const express = require('express')
 const path = require('path')
 const fs = require('fs-extra')
+const appModulePath = require('app-module-path')
+const Logger = require('roosevelt-logger')
 const certsGenerator = require('./lib/scripts/certsGenerator.js')
 const sessionSecretGenerator = require('./lib/scripts/sessionSecretGenerator.js')
 const csrfSecretGenerator = require('./lib/scripts/csrfSecretGenerator.js')
 
-module.exports = (options = {}, schema) => {
+const roosevelt = (options = {}, schema) => {
   options.appDir = options.appDir || path.dirname(module.parent.filename) // appDir is either specified by the user or sourced from the parent require
   const connections = {}
   const app = express() // initialize express
@@ -16,16 +18,14 @@ module.exports = (options = {}, schema) => {
   let initialized = false
   let checkConnectionsTimeout
   let persistProcess
-
-  // expose initial vars
-  app.set('express', express)
-  app.set('router', router)
+  let logger
+  let appName
+  let appEnv
 
   // source user-supplied params
-  const params = require('./lib/sourceParams')(options, app, schema)
-  const logger = app.get('logger')
-  const appName = app.get('appName')
-  const appEnv = app.get('env')
+  const params = require('./lib/sourceParams')(options, schema)
+  const appDir = params.appDir
+  const pkg = params.pkg
 
   // use existence of public folder to determine if this is the first run
   if (!fs.pathExistsSync(params.publicFolder) && params.logging.methods.info) {
@@ -34,142 +34,167 @@ module.exports = (options = {}, schema) => {
     require('./lib/scripts/deprecationCheck')(params.appDir)
   }
 
-  // app starting message
-  if (params.makeBuildArtifacts === 'staticsOnly') logger.info('💭', `Building ${appName} static site in ${appEnv} mode...`.bold)
-  else logger.info('💭', `Starting ${appName} in ${appEnv} mode...`.bold)
-
-  // generate express session secret
-  if (params.expressSession && params.makeBuildArtifacts !== 'staticsOnly') {
-    if (!fs.pathExistsSync(params.secretsDir) || !fs.pathExistsSync(params.secretsDir + '/sessionSecret.json')) {
-      sessionSecretGenerator()
-    }
-  }
-
-  // generate csrf secret
-  if (params.csrfProtection && params.makeBuildArtifacts !== 'staticsOnly') {
-    if (!fs.pathExistsSync(params.secretsDir) || !fs.pathExistsSync(params.secretsDir + '/csrfSecret.json')) {
-      csrfSecretGenerator()
-    }
-  }
-
-  // set up http server
-  if (!params.https.force || !params.https.enable) {
-    httpServer = require('http').Server(app)
-    httpServer.on('connection', mapConnections)
-  }
-
-  // setup https server if https is enabled
-  if (params.https.enable && params.makeBuildArtifacts !== 'staticsOnly') {
-    const authInfoPath = params.https.authInfoPath
-    const httpsOptions = {}
-
-    // runs the certGenerator if params.https.enable
-    if (params.https.autoCert && authInfoPath?.authCertAndKey) {
-      const { authCertAndKey } = authInfoPath
-
-      if ((!fs.pathExistsSync(params.secretsDir) || (!fs.pathExistsSync(authCertAndKey.key) || (!fs.pathExistsSync(authCertAndKey.cert))))) {
-        certsGenerator()
-      }
-    }
-
-    function isCertString (stringToTest) {
-      let testString = stringToTest
-      if (typeof testString !== 'string') {
-        testString = testString.toString()
-      }
-      const lastChar = testString.substring(testString.length - 1)
-      // A file path string won't have an end of line character at the end
-      // Looking for either \n or \r allows for nearly any OS someone could
-      // use, and a few that node doesn't work on.
-      if (lastChar === '\n' || lastChar === '\r') {
-        return true
-      }
-      return false
-    }
-
-    if (authInfoPath) {
-      if (authInfoPath.p12?.p12Path) {
-        // if the string ends with a dot and 3 alphanumeric characters (including _)
-        // then we assume it's a filepath.
-        if (typeof authInfoPath.p12.p12Path === 'string' && authInfoPath.p12.p12Path.match(/\.\w{3}$/)) {
-          httpsOptions.pfx = fs.readFileSync(path.join(params.appDir, params.secretsDir, authInfoPath.p12.p12Path))
-        } else { // if the string doesn't end that way, we assume it's an encrypted string
-          httpsOptions.pfx = authInfoPath.p12.p12Path
-        }
-      } else if (authInfoPath.authCertAndKey) {
-        function assignCertStringByKey (key) {
-          const { authCertAndKey } = authInfoPath
-          const certString = authCertAndKey[key]
-
-          if (isCertString(certString)) httpsOptions[key] = certString
-          else httpsOptions[key] = fs.readFileSync(path.join(params.appDir, params.secretsDir, certString))
-        }
-
-        if (authInfoPath.authCertAndKey.cert) assignCertStringByKey('cert')
-        if (authInfoPath.authCertAndKey.key) assignCertStringByKey('key')
-      }
-
-      // set passphrase if in use
-      if (params.https.passphrase) httpsOptions.passphrase = params.https.passphrase
-    }
-
-    if (params.https.caCert) {
-      if (typeof params.https.caCert === 'string') {
-        if (isCertString(params.https.caCert)) { // then it's the cert(s) as a string, not a file path
-          httpsOptions.ca = params.https.caCert
-        } else { // it's a file path to the file, so read file
-          httpsOptions.ca = fs.readFileSync(path.join(params.appDir, params.secretsDir, params.https.caCert))
-        }
-      } else if (params.https.caCert instanceof Array) {
-        httpsOptions.ca = []
-
-        for (const certOrPath of params.https.caCert) {
-          let certStr = certOrPath
-          if (!isCertString(certOrPath)) certStr = fs.readFileSync(certOrPath)
-          httpsOptions.ca.push(certStr)
-        }
-      }
-    }
-
-    httpsOptions.requestCert = params.https.requestCert
-    httpsOptions.rejectUnauthorized = params.https.rejectUnauthorized
-
-    httpsServer = require('https').Server(httpsOptions, app)
-    httpsServer.on('connection', mapConnections)
-  }
-
-  // expose http server(s) to the user via express var
-  app.set('httpServer', httpServer)
-  app.set('httpsServer', httpsServer)
-
-  // enable gzip compression
-  app.use(require('compression')())
-
-  // enable favicon support
-  if (params.favicon !== 'none' && params.favicon !== null) {
-    const faviconPath = path.join(params.staticsRoot, params.favicon)
-    if (fs.pathExistsSync(faviconPath)) app.use(require('serve-favicon')(faviconPath))
-    else logger.warn(`Favicon ${params.favicon} does not exist. Please ensure the "favicon" param is configured correctly.`)
-  }
-
-  // bind user-defined middleware which fires at the beginning of each request if supplied
-  if (params.onReqStart && typeof params.onReqStart === 'function') {
-    app.use(params.onReqStart)
-  }
-
-  // configure express, express-session, and csrf
-  require('./lib/setExpressConfigs')(app)
-
-  // fire user-defined onServerInit event
-  if (params.onServerInit && typeof params.onServerInit === 'function') {
-    params.onServerInit(app)
-  }
-
   // utility functions
 
   async function initServer () {
     if (initialized) return
     initialized = true
+
+    // configure logger with params
+    logger = new Logger(params.logging)
+
+    // expose express variables
+    app.set('express', express)
+    app.set('router', router)
+    app.set('env', params.mode === 'production-proxy' ? 'production' : params.mode)
+    app.set('logger', logger) // expose instance of roosevelt-logger module
+    app.set('params', params) // expose app configuration
+    app.set('appDir', appDir)
+    app.set('package', pkg) // expose contents of package.json if any
+    app.set('appName', pkg.name || 'Roosevelt Express')
+    app.set('appVersion', pkg.version)
+    app.set('routePrefix', params.routePrefix)
+    app.set('modelsPath', path.join(appDir, params.modelsPath))
+    app.set('viewsPath', path.join(appDir, params.viewsPath))
+    app.set('controllersPath', path.join(appDir, params.controllersPath))
+    app.set('staticsRoot', params.staticsRoot)
+    app.set('htmlPath', params.html.sourcePath)
+    app.set('htmlModels', params.html.models)
+    app.set('cssPath', params.css.sourcePath)
+    app.set('jsPath', params.js.sourcePath)
+    app.set('htmlRenderedOutput', params.html.output)
+    app.set('cssCompiledOutput', params.css.output)
+    app.set('clientViewsBundledOutput', params.clientViews.output)
+    app.set('isomorphicControllersListOutput', params.isomorphicControllers.output)
+    app.set('publicFolder', params.unversionedPublic)
+
+    // make the app directory requirable
+    appModulePath.addPath(appDir)
+
+    // make the models directory requirable
+    appModulePath.addPath(path.join(appDir, params.modelsPath, '../'))
+
+    // make the controllers directory requirable
+    appModulePath.addPath(path.join(appDir, params.controllersPath, '../'))
+
+    appName = app.get('appName')
+    appEnv = app.get('env')
+
+    // app starting message
+    if (params.makeBuildArtifacts === 'staticsOnly') logger.info('💭', `Building ${appName} static site in ${appEnv} mode...`.bold)
+    else logger.info('💭', `Starting ${appName} in ${appEnv} mode...`.bold)
+
+    // generate express session secret
+    if (params.expressSession && params.makeBuildArtifacts !== 'staticsOnly') {
+      if (!fs.pathExistsSync(params.secretsDir) || !fs.pathExistsSync(params.secretsDir + '/sessionSecret.json')) {
+        sessionSecretGenerator()
+      }
+    }
+
+    // generate csrf secret
+    if (params.csrfProtection && params.makeBuildArtifacts !== 'staticsOnly') {
+      if (!fs.pathExistsSync(params.secretsDir) || !fs.pathExistsSync(params.secretsDir + '/csrfSecret.json')) {
+        csrfSecretGenerator()
+      }
+    }
+
+    // set up http server
+    if (!params.https.force || !params.https.enable) {
+      httpServer = require('http').Server(app)
+      httpServer.on('connection', mapConnections)
+    }
+
+    // setup https server if https is enabled
+    if (params.https.enable && params.makeBuildArtifacts !== 'staticsOnly') {
+      const authInfoPath = params.https.authInfoPath
+      const httpsOptions = {}
+
+      // runs the certGenerator if params.https.enable
+      if (params.https.autoCert && authInfoPath?.authCertAndKey) {
+        const { authCertAndKey } = authInfoPath
+
+        if ((!fs.pathExistsSync(params.secretsDir) || (!fs.pathExistsSync(authCertAndKey.key) || (!fs.pathExistsSync(authCertAndKey.cert))))) {
+          certsGenerator()
+        }
+      }
+
+      if (authInfoPath) {
+        if (authInfoPath.p12?.p12Path) {
+          // if the string ends with a dot and 3 alphanumeric characters (including _)
+          // then we assume it's a filepath.
+          if (typeof authInfoPath.p12.p12Path === 'string' && authInfoPath.p12.p12Path.match(/\.\w{3}$/)) {
+            httpsOptions.pfx = fs.readFileSync(path.join(params.appDir, params.secretsDir, authInfoPath.p12.p12Path))
+          } else { // if the string doesn't end that way, we assume it's an encrypted string
+            httpsOptions.pfx = authInfoPath.p12.p12Path
+          }
+        } else if (authInfoPath.authCertAndKey) {
+          function assignCertStringByKey (key) {
+            const { authCertAndKey } = authInfoPath
+            const certString = authCertAndKey[key]
+
+            if (isCertString(certString)) httpsOptions[key] = certString
+            else httpsOptions[key] = fs.readFileSync(path.join(params.appDir, params.secretsDir, certString))
+          }
+
+          if (authInfoPath.authCertAndKey.cert) assignCertStringByKey('cert')
+          if (authInfoPath.authCertAndKey.key) assignCertStringByKey('key')
+        }
+
+        // set passphrase if in use
+        if (params.https.passphrase) httpsOptions.passphrase = params.https.passphrase
+      }
+
+      if (params.https.caCert) {
+        if (typeof params.https.caCert === 'string') {
+          if (isCertString(params.https.caCert)) { // then it's the cert(s) as a string, not a file path
+            httpsOptions.ca = params.https.caCert
+          } else { // it's a file path to the file, so read file
+            httpsOptions.ca = fs.readFileSync(path.join(params.appDir, params.secretsDir, params.https.caCert))
+          }
+        } else if (params.https.caCert instanceof Array) {
+          httpsOptions.ca = []
+
+          for (const certOrPath of params.https.caCert) {
+            let certStr = certOrPath
+            if (!isCertString(certOrPath)) certStr = fs.readFileSync(certOrPath)
+            httpsOptions.ca.push(certStr)
+          }
+        }
+      }
+
+      httpsOptions.requestCert = params.https.requestCert
+      httpsOptions.rejectUnauthorized = params.https.rejectUnauthorized
+
+      httpsServer = require('https').Server(httpsOptions, app)
+      httpsServer.on('connection', mapConnections)
+    }
+
+    // expose http server(s) to the user via express var
+    app.set('httpServer', httpServer)
+    app.set('httpsServer', httpsServer)
+
+    // enable gzip compression
+    app.use(require('compression')())
+
+    // enable favicon support
+    if (params.favicon !== 'none' && params.favicon !== null) {
+      const faviconPath = path.join(params.staticsRoot, params.favicon)
+      if (fs.pathExistsSync(faviconPath)) app.use(require('serve-favicon')(faviconPath))
+      else logger.warn(`Favicon ${params.favicon} does not exist. Please ensure the "favicon" param is configured correctly.`)
+    }
+
+    // bind user-defined middleware which fires at the beginning of each request if supplied
+    if (params.onReqStart && typeof params.onReqStart === 'function') {
+      app.use(params.onReqStart)
+    }
+
+    // configure express, express-session, and csrf
+    require('./lib/setExpressConfigs')(app)
+
+    // fire user-defined onServerInit event
+    if (params.onServerInit && typeof params.onServerInit === 'function') {
+      await Promise.resolve(params.onServerInit(app))
+    }
 
     require('./lib/generateSymlinks')(app)
 
@@ -192,7 +217,6 @@ module.exports = (options = {}, schema) => {
     }
 
     // map routes
-    // TODO: why was app being redefined here?
     require('./lib/mapRoutes')(app)
 
     // custom error page
@@ -201,7 +225,6 @@ module.exports = (options = {}, schema) => {
       require(params.errorPages.internalServerError)(app, err, req, res)
     })
 
-    // TODO: why is this here? there doesn't appear to be anything related to static assets being generated in this general area
     if (params.onStaticAssetsGenerated && typeof params.onStaticAssetsGenerated === 'function') {
       params.onStaticAssetsGenerated(app)
     }
@@ -377,13 +400,33 @@ module.exports = (options = {}, schema) => {
     }
   }
 
+  function isCertString (stringToTest) {
+    let testString = stringToTest
+    if (typeof testString !== 'string') {
+      testString = testString.toString()
+    }
+    const lastChar = testString.substring(testString.length - 1)
+    // A file path string won't have an end of line character at the end
+    // Looking for either \n or \r allows for nearly any OS someone could
+    // use, and a few that node doesn't work on.
+    if (lastChar === '\n' || lastChar === '\r') {
+      return true
+    }
+    return false
+  }
+
   return {
+    expressApp: app,
+    // TODO: Since both of these server objects are stored in express variables we may be able to remove them from here
     httpServer,
     httpsServer,
-    expressApp: app,
     initServer,
     init: initServer,
     startServer,
-    stopServer: shutdownGracefully
+    start: startServer,
+    stopServer: shutdownGracefully,
+    stop: shutdownGracefully
   }
 }
+
+module.exports = roosevelt
